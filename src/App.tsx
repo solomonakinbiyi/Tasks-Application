812c84841b6b87fa3a94992fb7058004bae19b06
solomonakinbiyi <akinbiyisolo22@gmail.com>
2022-11-11 23:10:14 +0100

    dashboard created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { FC, ReactElement } from "react";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { customTheme } from "./theme/customTheme";
+import { Dashbaord } from "./pages/dashboard/dashbaord";
 
 const App: FC = (): ReactElement => {
   return (
     <ThemeProvider theme={customTheme}>
       <CssBaseline />
+      <Dashbaord />
       Ready to build 🚀🔥
     </ThemeProvider>
   );
--- src/pages/dashboard/dashbaord.tsx
+++ src/pages/dashboard/dashbaord.tsx
@@ -1,5 +1,30 @@
 import React, { FC, ReactElement } from "react";
+import { Grid } from "@mui/material";
 
 export const Dashbaord: FC = (): ReactElement => {
-  return <div>dashbaord</div>;
+  return (
+    <Grid container minHeight="100vh" p={0} m={0}>
+      <Grid item md={8} px={4}>
+        <h1>Content area</h1>
+      </Grid>
+      <Grid
+        item
+        md={4}
+        sx={{
+          height: "100vh",
+          position: "fixed",
+          right: 0,
+          top: 0,
+          width: "100%",
+          backgroundColor: "background.paper",
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <h1>Sidebar Arena</h1>
+      </Grid>
+    </Grid>
+  );
 };
