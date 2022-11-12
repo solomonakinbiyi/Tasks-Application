908753f4b4ee4672654156ab39b600ecd4cfbbf9
solomonakinbiyi <akinbiyisolo22@gmail.com>
2022-11-12 12:46:27 +0100

    created interfaces for taskSelectField

--- src/components/createtaskform/_taskSelectField.tsx
+++ src/components/createtaskform/_taskSelectField.tsx
@@ -7,20 +7,34 @@ import {
   Select,
   SelectChangeEvent,
 } from "@mui/material";
+import { ISelectField } from "./interfaces/ISelectField";
 
-export const TaskSelectField: FC = (props): ReactElement => {
+export const TaskSelectField: FC<ISelectField> = (props): ReactElement => {
+  const {
+    value = "",
+    label = "Select box",
+    name = "Select box",
+    items = [{ value: "", label: "Add Items" }],
+    disabled = false,
+    onChange = (e: SelectChangeEvent) => console.log(e),
+  } = props;
   return (
     <FormControl fullWidth size="small">
-      <InputLabel id="status">Status</InputLabel>
+      <InputLabel id={`${name}-id`}>{label}</InputLabel>
       <Select
-        labelId="status"
-        id="status-select"
-        value=""
-        label="Status"
-        name="status"
+        labelId={`${name}-id`}
+        id={`${name}-id-select`}
+        value={`${value}`}
+        label={`${label}`}
+        name={`${name}`}
+        onChange={onChange}
+        disabled={disabled}
       >
-        <MenuItem value={10}>10</MenuItem>
-        <MenuItem value={20}>20</MenuItem>
+        {items.map((item, index) => (
+          <MenuItem value={item.value} key={item.value + index}>
+            {item.label}
+          </MenuItem>
+        ))}
       </Select>
     </FormControl>
   );
--- src/theme/customTheme.ts
+++ src/theme/customTheme.ts
@@ -1,16 +1,16 @@
-import {createTheme, ThemeOptions} from '@mui/material';
+import { createTheme, ThemeOptions } from "@mui/material";
 
 export const customTheme: ThemeOptions = createTheme({
-    palette: {
-        mode: "dark",
-        primary: {
-            light: 'rgba(168,85,247,.80)',
-            main: 'rgba(168,85,247,.65)',
-            dark: 'rgba(168,85,247,.28)'
-        },
-        background: {
-            paper: '#151515',
-            default: 'rgba(0,0,0,.96)'
-        }
-    }
-})
+  palette: {
+    mode: "dark",
+    primary: {
+      light: "rgba(168,85,247,.80)",
+      main: "rgba(168,85,247,.65)",
+      dark: "rgba(168,85,247,.28)",
+    },
+    background: {
+      paper: "#151515",
+      default: "rgba(0,0,0,.96)",
+    },
+  },
+});
