55aeb652c8390e1fb530d068717767fee7218a07
solomonakinbiyi <akinbiyisolo22@gmail.com>
2022-11-12 09:38:22 +0100

    making components dynamic v2 ✅

--- src/components/createtaskform/_taskDescriptionField.tsx
+++ src/components/createtaskform/_taskDescriptionField.tsx
@@ -1,6 +1,6 @@
 import { TextField } from "@mui/material";
 import React, { FC, ReactElement } from "react";
-import { ITextField } from "./interfaces/ITextfield";
+import { ITextField } from "./interfaces/ITextField";
 
 export const TaskDescriptionField: FC<ITextField> = (props): ReactElement => {
   const { onChange = (e) => console.log(e), disabled = false } = props;
