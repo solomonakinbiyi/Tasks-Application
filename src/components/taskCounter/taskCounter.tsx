218ff396f39f5bbdc0b40498c23f5fe77f8de641
solomonakinbiyi <akinbiyisolo22@gmail.com>
2022-11-14 12:36:34 +0100

    styling task counter

--- src/components/taskCounter/taskCounter.tsx
+++ src/components/taskCounter/taskCounter.tsx
@@ -9,10 +9,28 @@ export const TaskCounter: FC = (): ReactElement => {
       justifyContent="center"
       alignItems="center"
     >
-      <Avatar>
-        <Typography>10</Typography>
+      <Avatar
+        sx={{
+          backgroundColor: "transparent",
+          border: "5px solid",
+          width: "96px",
+          height: "96px",
+          marginBottom: "16px",
+          borderColor: "warning.light",
+        }}
+      >
+        <Typography color="#ffffff" variant="h4">
+          10
+        </Typography>
       </Avatar>
-      <Typography>Subtitle</Typography>
+      <Typography
+        color="#ffffff"
+        fontWeight="bold"
+        fontSize="20px"
+        variant="h5"
+      >
+        Subtitle
+      </Typography>
     </Box>
   );
 };
