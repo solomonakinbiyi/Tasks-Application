5dd47cb88ebbc5b9040aaadf6d86824f69552aa8
solomonakinbiyi <akinbiyisolo22@gmail.com>
2022-11-14 16:10:06 +0100

    made taskfooter dynamic

--- src/components/task/_taskFooter.tsx
+++ src/components/task/_taskFooter.tsx
@@ -1,7 +1,14 @@
 import React, { FC, ReactElement } from "react";
 import { Box, Button, FormControlLabel, Switch } from "@mui/material";
+import { ITaskFooter } from "./interfaces/ITaskFooter";
+import PropTypes from "prop-types";
 
-export const TaskFooter: FC = (): ReactElement => {
+export const TaskFooter: FC<ITaskFooter> = (props): ReactElement => {
+  const {
+    onStatusChange = (e) => console.log(e),
+    onClick = (e) => console.log(e),
+  } = props;
+
   return (
     <Box
       display="flex"
@@ -11,16 +18,22 @@ export const TaskFooter: FC = (): ReactElement => {
     >
       <FormControlLabel
         label="In Progress"
-        control={<Switch color="warning" />}
+        control={<Switch onChange={(e) => onStatusChange(e)} color="warning" />}
       />
       <Button
         variant="contained"
         color="success"
         size="small"
         sx={{ color: "#ffffff" }}
+        onClick={(e) => onClick(e)}
       >
         Mark Complete
       </Button>
     </Box>
   );
+};
+
+TaskFooter.propTypes = {
+  onStatusChange: PropTypes.func,
+  onClick: PropTypes.func,
 };
