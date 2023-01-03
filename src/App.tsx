4f7abd12f71f7fd8995c0e1b80d6fa1c0bef517b
solomonakinbiyi <akinbiyisolo22@gmail.com>
2023-01-03 22:05:42 +0100

    installed tansack query

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import React, { FC, ReactElement } from "react";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { customTheme } from "./theme/customTheme";
 import { Dashbaord } from "./pages/dashboard/dashbaord";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 const App: FC = (): ReactElement => {
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline />
-      <Dashbaord />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <Dashbaord />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
