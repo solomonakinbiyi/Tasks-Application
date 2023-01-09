08df5f79f4060b1e43187e957d1e3b7f324b9e85
solomonakinbiyi <akinbiyisolo22@gmail.com>
2023-01-09 15:26:40 +0100

    markCompleteHandler implemented

--- src/components/taskarea/taskarea.tsx
+++ src/components/taskarea/taskarea.tsx
@@ -33,6 +33,18 @@ export const TaskArea: FC = (): ReactElement => {
     });
   }
 
+  function markCompleteHandler(
+    e:
+      | React.MouseEvent<HTMLButtonElement>
+      | React.MouseEvent<HTMLAnchorElement>,
+    id: string
+  ) {
+    updateTaskMutation.mutate({
+      id,
+      status: Status.completed,
+    });
+  }
+
   return (
     <Grid item md={8} px={4}>
       <Box mb={8} px={4}>
@@ -85,6 +97,7 @@ export const TaskArea: FC = (): ReactElement => {
                     priority={each.priority}
                     status={each.status}
                     onStatusChange={onStatusChangeHandler}
+                    onClick={markCompleteHandler}
                   />
                 ) : (
                   false
